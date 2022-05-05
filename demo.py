71db73bb19e323098782188a3064bf5fad491f38
rahulraitcs <rai.rahul2@tcs.com>
2022-05-05 19:52:13 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/hello')
+def hello_world():
+    return 'Hello world!'
+
+
+@app.route('/demo/<name>/<int:age>')
+def demo(name, age):
+    print(age)
+    return 'Hello '+name+', Welcome to TCS'
+
+#app.add_url_rule('/demo/<name>/<int:age>', 'demo', demo)
+
